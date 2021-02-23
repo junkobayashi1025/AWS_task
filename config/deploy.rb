ecf27244293f76120277213951c96d8936d4737d
jun kobayashi <junkobayashi1025@gmail.com>
2021-02-23 10:05:39 +0000

    unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,11 +4,11 @@ lock '3.6.0'
 set :application, 'achieve'
 # cloneするgitのレポジトリ
 # （xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/junkobayashi1025/manyoh_task.git'
+set :repo_url, 'https://github.com/junkobayashi1025/AWS_task.git'
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/manyoh_task'
+set :deploy_to, '/var/www/achieve'
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
